db80d9663a20cbb79a719ee4ee5f97ee33902b57
johnny zhong <johnnyczhong@gmail.com>
2019-10-06 12:39:04 -0700

    added dask examples

--- Midas/DataPreProcessing/main.py
+++ Midas/DataPreProcessing/main.py
@@ -1,1 +1,13 @@
-print('hello world')
+import dask.dataframe as dd
+import time
+
+start_time = time.time()
+train_transaction = dd.read_csv('train_transaction.csv')
+train_id = dd.read_csv('train_identity.csv')
+
+joined_data = train_transaction.merge(train_id, how='left', on='TransactionID')
+print(f'Took {time.time() - start_time}')
+
+print(f'{joined_data.shape}')
+print(f'{joined_data.head()}')
+print(f'Took {time.time() - start_time}')

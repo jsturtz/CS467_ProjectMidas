af12698620db252fadb0cffc1c9595b0bb6cb733
Jordan Sturtz <jordan.sturtz@centralsquare.com>
2019-10-24 08:29:12 -0400

    Added data upload functionality to train.js and train.html

--- Midas/MidasWebsite/static/js/train.js
+++ Midas/MidasWebsite/static/js/train.js
@@ -1,8 +1,24 @@
-// Binds page selector when DOM loads
 $(document).ready(function(){
+
+  // controls which page is displayed when user selects dropdown
   $(".dropdown-item.training-type").click(function(e){
     $(".container.training_type").hide();
     $("#" + e.currentTarget.name).show();
     $("#screen_title").text(e.currentTarget.textContent);
   })
+
+  // checks whether input extension is valid
+  $("#raw-data-input").change(function (e) {
+    var fileExtension = ['csv'];
+    if ($.inArray($(this).val().split('.').pop().toLowerCase(), fileExtension) == -1) {
+        alert("Only formats allowed : " + fileExtension.join(', '));
+        $("#raw-data-form")[0].reset();
+    }
+    else {
+      // the e.currentTarget.value will hold path we need
+      // pass this to backend business logic when we get that figured out
+      console.log(e.currentTarget.value);
+    }
+  });
 });
+

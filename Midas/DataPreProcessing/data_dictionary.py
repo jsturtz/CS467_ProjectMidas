e79e3e85376120026f9428e1e5145691f0e12897
esOSU <esOSU@users.noreply.github.com>
2019-10-24 18:11:01 -0700

    Fixed typos

--- Midas/DataPreProcessing/data_dictionary.py
+++ Midas/DataPreProcessing/data_dictionary.py
@@ -3,56 +3,55 @@ import pandas as pd
 import numpy as np
 
 
-def make_data_dictionary(in_df, out_html_file):
+def make_data_dictionary(in_data, out_html_file):
 
     # create a new dataframe for the data dictionary containing the feature list
-    dd = pd.DataFrame(list(dfname),columns=['Feature'])
+    dd = pd.DataFrame(list(in_data),columns=['Feature'])
     
     #  add variable type to the data dictionary
-    dd['Type'] = dfname.dtypes.tolist()
+    dd['Type'] = in_data.dtypes.tolist()
     
     # add count of distinct values to data dictionary
-    dd['Distinct'] = dfname.nunique().tolist()
+    dd['Distinct'] = in_data.nunique().tolist()
     
     # make lists for frequency counts and missing values
     freq_counts = []
     missing_counts = []
     
     # iterate through the columns
-    for column in dfname:
+    for column in in_data:
         # get frequency counts
-        vcs = dfname[column].value_counts(dropna=False).to_dict()
+        vcs = in_data[column].value_counts(dropna=False).to_dict()
         
         # use binning if the feature is numeric and there are many unique values
-        if len(vcs) > 10 and np.issubdtype(dfname[column].dtype,np.number):
-            bins_data = dfname[column].value_counts(dropna=False, bins = 10)
+        if len(vcs) > 10 and np.issubdtype(in_data[column].dtype,np.number):
+            bins_data = in_data[column].value_counts(dropna=False, bins = 10)
             freq_counts.extend([(bins_data.to_dict())])
             
         # if many unique values, but categorical variable, only keep top 10 frequencies
         elif len(vcs) > 10:
-            freq_counts.extend([dfname[column].value_counts(dropna=False)[:10].to_dict()])
+            freq_counts.extend([in_data[column].value_counts(dropna=False)[:10].to_dict()])
         
         # for a small number of unique values, keep all frequencies
         else:
             freq_counts.extend([vcs])
             
         # obtain count of missing values    
-        if np.issubdtype(dfname[column].dtype,np.number):
+        if np.issubdtype(in_data[column].dtype,np.number):
             # for numeric variables, use isnull
-            missing_counts.extend([dfname[column].isnull().sum()])
+            missing_counts.extend([in_data[column].isnull().sum()])
         else:
-            missing_counts.extend([dfname[column].isna().sum()])
-            #missing_counts.extend([(dfname[column].values == '').sum()])
-    
+            missing_counts.extend([in_data[column].isna().sum()])
+   
     # add count of missing values
     dd['Missing'] = missing_counts
         
     # Add minimum value for each feature (if applicable)
-    dd = dd.merge(dfname.min().to_frame(), left_on=['Feature'], right_index=True, how='left')
+    dd = dd.merge(in_data.min().to_frame(), left_on=['Feature'], right_index=True, how='left')
     dd.rename(columns={dd.columns[-1]: 'Minimum'}, inplace=True)
     
     # Add maximum value for each feature (if applicable)
-    dd= dd.merge(dfname.max().to_frame(), left_on=['Feature'], right_index=True, how='left')
+    dd= dd.merge(in_data.max().to_frame(), left_on=['Feature'], right_index=True, how='left')
     dd.rename(columns={dd.columns[-1]: 'Maximum'}, inplace=True)
         
     dd['Top Frequencies'] = freq_counts
@@ -64,9 +63,9 @@ def make_data_dictionary(in_df, out_html_file):
     return dd[1:].to_html(out_html_file)
 
 # load sample data
-#train_transaction = dd.read_csv('train_transaction.csv')
-#train_id = dd.read_csv('train_identity.csv')
+train_transaction = pd.read_csv('./transaction_small.csv')
+train_id = pd.read_csv('identity_small.csv')
     
 # generate data dictionary
-#make_data_dictionary(train_transaction, 'data_dictionary_transaction.html')
-#make_data_dictionary(train_id, 'data_dictionary_transaction.html')
+make_data_dictionary(train_transaction, 'data_dictionary_transaction.html')
+make_data_dictionary(train_id, 'data_dictionary_id.html')

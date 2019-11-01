a79035d8ef28d1d96638dbd9c89268a97caa736e
johnnyczhong <johnny@ridecell.com>
2019-11-01 13:31:23 -0700

    added database script

---
+++ Midas/database.py
@@ -0,0 +1,49 @@
+"""
+Description: Bring up databases.
+Input: None
+Output: None
+"""
+def databases_up():
+    import subprocess
+    subprocess.run(['make'], cwd="./Databases")
+
+
+"""
+Description:
+    Will attempt to retrieve a model from our model database
+Input:
+    None
+
+Output:
+    Will return some kind of data structure for the model (fill this out in development)
+"""
+
+def get_model():
+    return None
+
+"""
+Description:
+    Will store the model data structure into our model database
+
+Input:
+    Some model data structure (fill this out in development)
+
+Output:
+    None
+"""
+
+def store_model(model):
+    return
+
+"""
+Description:
+    Will store cleanly formatted data into database
+
+Input:
+    Some data structure holding data? Figure out in development
+
+Output:
+"""
+
+def store_clean_data(model):
+    return
